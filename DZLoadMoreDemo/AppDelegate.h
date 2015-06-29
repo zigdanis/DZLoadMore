fe47560f760d5ef48b3561647a73889c4281022a
Danis Ziganshin <zigdanis@gmail.com>
2015-06-29 09:35:16 +0300

    Initial Commit

---
+++ DZLoadMoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DZLoadMoreDemo
+//
+//  Created by Danis Ziganshin on 29/06/15.
+//  Copyright (c) 2015 Danis Ziganshin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
